4524799b30061212e9126d074a4610b52f45e021
Viary17 <nindiaoktavia83@gmail.com>
2024-12-11 02:02:30 +0700

    rubah footer

--- app/Views/layouts/frontend/header.php
+++ app/Views/layouts/frontend/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="/css/header.css" rel="stylesheet">
+    <link href="assets/css/header.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css">
     <link href="<?= base_url('css/header.css'); ?>" rel="stylesheet">
 
