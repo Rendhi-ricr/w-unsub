28a5b77a6a0a841cf9e5dafa6c5026d9786778c4
Tiaftrn <tiafitriana2012@gmail.com>
2024-12-11 17:38:06 +0700

    baru

--- app/Views/layouts/frontend/template.php
+++ app/Views/layouts/frontend/template.php
@@ -3,11 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
     <link href="assets/css/home.css" rel="stylesheet">
-=======
     <link href="<?= base_url('assets/css/frontend/home.css'); ?>" rel="stylesheet">
->>>>>>> 7693390fc7b8d6b92898d3f776f60cc3a6338db5
     <link rel="stylesheet" type="text/css" href="css/sb-admin-2.min.css">
     <?= $this->renderSection('css'); ?> 
     <link href="<?= base_url('assets/css/frontend/sejarah.css'); ?>" rel="stylesheet">
