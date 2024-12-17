b5b7837a63b998e754793692de7f58b1d1fb1318
Viary17 <nindiaoktavia83@gmail.com>
2024-12-17 21:22:57 +0700

    up

--- app/Views/layouts/frontend/footer.php
+++ app/Views/layouts/frontend/footer.php
@@ -30,12 +30,12 @@
             <div class="row">
                 <div class="col-md-6 mb-3">
                     <h5>Kampus I</h5>
-                    <img src="foto/kampus1.png" alt="Kampus I" style="max-width: 300px;">
+                    <img src="<?= base_url('foto/kampus1.png');?>" alt="Kampus I" style="max-width: 300px;">
                     <p>Jl. R.A. Kartini No. 4 Subang, Kabupaten Subang, Jawa Barat, Indonesia</p>
                 </div>
                 <div class="col-md-6 mb-3">
                     <h5>Kampus II</h5>
-                    <img src="foto/kampus2.png" alt="Kampus II" style="max-width: 300px;">
+                    <img src="<?= base_url('foto/kampus2.png');?>" alt="Kampus II" style="max-width: 300px;">
                     <p>Jl. Arief Rahman Hakim No. 8, Cibogo, Kabupaten Subang, Jawa Barat, Indonesia</p>
                 </div>
             </div>
